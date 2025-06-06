624d66b63003e142ee88d0f530920bd56350c2df
Maga <mcmagakrytoy@gmail.com>
2025-06-06 15:55:05 +0300

    создание файла app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+    abort,
+    flash,
+    session)
+import os
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "SHPI"
+app.config["UPLOAD_FOLDER"] = "uploads/"
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run(debug=True, port=8080)
