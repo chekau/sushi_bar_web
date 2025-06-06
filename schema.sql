53f7b84f0e450b29a44ac7e7b5687cab5c3be50e
Maga <mcmagakrytoy@gmail.com>
2025-06-06 21:40:16 +0300

    исправил синтаксические ошибки

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Dish (
-        `name` TEXT NOT NULL,
-        `description` TEXT NOT NULL
-        `image` TEXT NOT NULL
-        `price` INTEGER
+        name TEXT NOT NULL,
+        description TEXT NOT NULL,
+        image TEXT NOT NULL,
+        price INTEGER
 
 
-)
+);
